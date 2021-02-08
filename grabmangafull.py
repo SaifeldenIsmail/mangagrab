8e397f913453635f511375f1d5e12270d2edfef7
SaifeldenIsmail <52354962+SaifeldenIsmail@users.noreply.github.com>
2021-02-08 11:46:21 +0200

    Add files via upload

--- grabmangafull.py
+++ grabmangafull.py
@@ -1,11 +1,12 @@
-import time
-import subprocess
+from time import sleep
+from subprocess import run
 import sys
-import os
-import requests
-import bs4
+import argparse
+from os import chdir, listdir
+from requests import get
+from bs4 import BeautifulSoup
 from fpdf import FPDF
-import PIL
+from PIL import Image 
 
 def pick_option(options):
     for index, option in enumerate(options):
@@ -19,22 +20,19 @@ def pick_option(options):
             print('please pick a valid number. ')
 
 def query(search):
-    manga=requests.get(f'https://kissmanga.org/manga_list?q={search}&action=search').text
-    manga_query=bs4.BeautifulSoup(manga,features='lxml')
+    manga=get(f'https://kissmanga.org/manga_list?q={search}&action=search').text
+    manga_query=BeautifulSoup(manga,features='lxml')
     grab_top=manga_query.find_all('a',{'class':'item_movies_link'})
     option=pick_option(grab_top)
     rec=f'https://kissmanga.org{option["href"]}'
     return rec
 
-def iterate(search,x):
-    rec=query(search)
+def iterate(rec,x):
     chapter_link=f'{rec}/chapter_{x}'
     return chapter_link 
 
-def recent_chapter(search):
-    re=query(search)
-    rec=requests.get(re).text
-    rec_part=bs4.BeautifulSoup(rec,features='lxml').find_all('a')
+def recent_chapter(rec):
+    rec_part=BeautifulSoup(get(rec).text,features='lxml').find_all('a')
     cc=[]
     for i in rec_part:
         try:
@@ -52,9 +50,9 @@ def recent_chapter(search):
 
 def grab_links(recent):
       
-    raw_website=requests.get(recent.replace('manga','chapter',2).replace('chapter','manga',1)).text
+    raw_website=get(recent.replace('manga','chapter',2).replace('chapter','manga',1)).text
 
-    website=bs4.BeautifulSoup(raw_website,features="lxml")
+    website=BeautifulSoup(raw_website,features="lxml")
     chapter_tags=website.find_all('div',{'id':'centerDivVideo'})
     image_tags=chapter_tags[0].find_all('img')
     image_links=[]
@@ -64,21 +62,21 @@ def grab_links(recent):
     return image_links
 
 def download_images(image_links,chapter):
-    os.chdir('manga')
-    chapter_directory=f'{sys.argv[1]}/{sys.argv[1]}_{chapter}'
-    subprocess.run(['mkdir','-p',chapter_directory])
+    chdir('manga')
+    chapter_directory=f'{args.manga}/{args.manga}_{chapter}'
+    run(['mkdir','-p',chapter_directory])
     i=0
 
     for link in image_links:
-        subprocess.run(['wget','-O',f'{chapter_directory}/{i}.jpg',link])
-        time.sleep(0.1)
+        run(['wget','-O',f'{chapter_directory}/{i}.jpg',link])
+        sleep(0.1)
         i+=1
     return chapter_directory
 
 def convert_to_pdf(chapter_directory,chapter):
-    image_list=os.listdir(chapter_directory)
+    image_list=listdir(chapter_directory)
     image_list.sort(key=lambda x: int(x[:-4]))
-    dimensions= PIL.Image.open(f'{chapter_directory}/{image_list[2]}').size
+    dimensions= Image.open(f'{chapter_directory}/{image_list[2]}').size
     mmw=int(dimensions[0]/3.7795275591)
     mmh=int(dimensions[1]/3.7795275591)
     pdf=FPDF('P', 'mm', (mmw,mmh))
@@ -86,7 +84,7 @@ def convert_to_pdf(chapter_directory,chapter):
     
     for image in image_list:
         image_directory=f'{chapter_directory}/{image}'
-        image_dimensions=PIL.Image.open(image_directory).size
+        image_dimensions=Image.open(image_directory).size
         if image_dimensions[0]<image_dimensions[1]:
             pdf.add_page()
             pdf.image(name=image_directory, x = -0, y = -0, w =mmw , h = 0, type = '', link = '')
@@ -94,48 +92,48 @@ def convert_to_pdf(chapter_directory,chapter):
             pdf.add_page('L')
             pdf.image(name=image_directory, x = 0, y = -0, w =mmh , h = 0, type = '', link = '')
     
-    os.chdir(sys.argv[1])
-    pdf.output(f'{sys.argv[1]}_chapter_{chapter}.pdf', 'F')
+    chdir(args.manga)
+    pdf.output(f'{args.manga}_chapter_{chapter}.pdf', 'F')
     
-    subprocess.run(['rm','-r',f'{sys.argv[1]}_{chapter}'])
-    os.chdir('../../')
+    run(['rm','-r',f'{args.manga}_{chapter}'])
+    chdir('../../')
 
-#running code below
-
-#try:
-#except:
-#    chapter=recent_chapter()
-#
-#image_links=grab_links(chapter)
-#chapter_directory=download_images(image_links,chapter)
-#convert_to_pdf(chapter_directory)
 def main(recent_link,chapter):
     image_links=grab_links(recent_link)
     chapter_directory=download_images(image_links,chapter)
     convert_to_pdf(chapter_directory,chapter)
 
 
-if len(sys.argv)==2:
-    recent_link,chapter=recent_chapter(sys.argv[1])
-    main(recent_link,chapter)
 
-elif len(sys.argv)==3:
-    chapter=sys.argv[2]
-    recent_link=iterate(sys.argv[1],chapter)
-    main(recent_link,chapter)
+parser=argparse.ArgumentParser()
 
-elif len(sys.argv)==4:
+parser.add_argument('--manga','-m',required=True)
 
-    for chapter in range(int(sys.argv[2]),int(sys.argv[3])+1):
-        recent_link=iterate(sys.argv[1],chapter)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--through','-t',nargs=2,type=int)
+group.add_argument('--single','-s',nargs='*')
+group.add_argument('--recent','-r',action='store_true')
+
+args = parser.parse_args()
+
+
+if args.single:
+
+    rec=query(args.manga)
+    for single_chapter in args.single:
+        chapter=single_chapter
+        recent_link=iterate(rec,chapter)
         main(recent_link,chapter)
 
-else:
-    print('updating list')
+if args.through:
 
+    rec=query(args.manga)
+    for chapter in range(args.through[0],args.through[1]+1):
+        recent_link=iterate(rec,chapter)
+        main(recent_link,chapter)
 
-    
-
-
-
+if args.recent:
+    rec=query(args.manga)
+    recent_link,chapter=recent_chapter(rec)
+    main(recent_link,chapter)
 
